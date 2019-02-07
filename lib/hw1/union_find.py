8308adbf3eb8d443091e0b6ec9b1ffd504755caa
Damien Levitt <damien.levitt@sjsu.edu>
2019-02-07 12:27:56 -0800

    found problem in logic, needed an extra get root function for when the path is compressed

--- lib/hw1/union_find.py
+++ lib/hw1/union_find.py
@@ -38,7 +38,7 @@ class UF(object):
 
         for x in range(len(self.id)):
                 if self.id[x] == pid:           # if the id at x is equal to the id at p then x and p are connected
-                    self.id[x] = qid            # there for you can set x to q
+                    self.id[x] = qid            # therefore you can set x to q
 
     def qf_connected(self, p, q):
         """Find operation for Quick-Find Algorithm.
@@ -85,13 +85,19 @@ class UF(object):
          """
         return self.get_root(p) == self.get_root(q)
 
+    def get_root_pqu(self, i):
+            while i != self.id[i]:
+                self.id[i] = self.id[self.id[i]]
+                i = self.id[i]
+            return i
+
     def pqu_union(self, p, q):
         """Union operation for path compressed Quick-Union Algorithm.
          connect p and q.
 
          """
-        root_of_p = self.get_root(p)
-        self.id[root_of_p] = self.get_root(q)
+        root_of_p = self.get_root_pqu(p)
+        self.id[root_of_p] = self.get_root_pqu(q)
 
     def pqu_connected(self, p, q):
         """Find operation for path compressed Quick-Union Algorithm.
@@ -99,7 +105,7 @@ class UF(object):
 
          """
 
-        return self.get_root(p) == self.get_root(q)
+        return self.get_root_pqu(p) == self.get_root_pqu(q)
 
     def wpqu_union(self, p, q):
         """Union operation for Weighted path compressed Quick-Union Algorithm.
@@ -107,12 +113,12 @@ class UF(object):
 
          """
 
-        if self.id[self.get_root(p)] < self.id[self.get_root(q)]:
-            self.id[self.get_root(p)] = self.get_root(q)
-            self.id[self.get_root(q)] += self.id[self.get_root(p)]
+        if self.id[self.get_root_pqu(p)] < self.id[self.get_root_pqu(q)]:
+            self.id[self.get_root_pqu(p)] = self.get_root_pqu(q)
+            self.id[self.get_root_pqu(q)] += self.id[self.get_root_pqu(p)]
         else:
-            self.id[self.get_root(q)] = self.get_root(p)
-            self.id[self.get_root(p)] += self.id[self.get_root(q)]
+            self.id[self.get_root_pqu(q)] = self.get_root_pqu(p)
+            self.id[self.get_root_pqu(p)] += self.id[self.get_root_pqu(q)]
 
     def wpqu_connected(self, p, q):
         """Find operation for Weighted path compressed Quick-Union Algorithm.
@@ -120,7 +126,7 @@ class UF(object):
 
          """
 
-        return self.get_root(p) == self.get_root(q)
+        return self.get_root_pqu(p) == self.get_root_pqu(q)
 
 
 if __name__ == "__main__":
@@ -135,7 +141,6 @@ if __name__ == "__main__":
         # initialize network nodes
         inodes = UF()
         inodes.qf_init(set_sz)
-
         t0 = time.time()
 
         for idx in range(set_sz - 1):
