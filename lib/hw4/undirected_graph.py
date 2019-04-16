8c9d917315402afacd6909d5c60ce9b03769a02c
Damien Levitt <damien.levitt@sjsu.edu>
2019-04-16 00:22:33 -0700

    undirected graph file is completed

--- lib/hw4/undirected_graph.py
+++ lib/hw4/undirected_graph.py
@@ -34,28 +34,30 @@ class Graph:
     def add_vertex(self, vertex):
         if isinstance(vertex, Vertex):
             self.vertices[vertex.name] = vertex.neighbors
-        else:
-            return 1
 
     def add_vertices(self, vertices):
         for vertex in vertices:
             if isinstance(vertex, Vertex):
                 self.vertices[vertex.name] = vertex.neighbors
-        else:
-            return 1
             
     def add_edge(self, vertex_from, vertex_to):
-        
-        return 1 
-                
+        if isinstance(vertex_from, Vertex) and isinstance(vertex_to, Vertex):
+            vertex_from.add_neighbor(vertex_to)
+            if isinstance(vertex_from, Vertex) and isinstance(vertex_to, Vertex):
+                self.vertices[vertex_from.name] = vertex_from.neighbors
+                self.vertices[vertex_to.name] = vertex_to.neighbors
+
     def add_edges(self, edges):
-        
-        return 1         
+        for edge in edges:
+            self.add_edge(edge[0], edge[1])
     
-    def adjacencyList(self): # to represent the graph as adjacent list  
-        
-        return 1           
-                        
+    def adjacencyList(self):  # to represent the graph as adjacent list
+        if len(self.vertices) >= 1:
+            return [str(key) + ":" + str(self.vertices[key]) for key in self.vertices.keys()]
+        else:
+            return dict()
+
+
 def graph(g):
     """ Function to print a graph as adjacency list and adjacency matrix. """
     return str(g.adjacencyList()) + '\n'
