f903368f415bf25a6f0d36f84f3085f3e53bcde3
Damien Levitt <damien.levitt@sjsu.edu>
2019-04-18 01:04:29 -0700

    finished bfs moving on to next py file

--- lib/hw4/dfs_bfs.py
+++ lib/hw4/dfs_bfs.py
@@ -2,25 +2,31 @@ from collections import defaultdict
 
 
 class Graph:
-	def __init__(self): 
-		self.graph = defaultdict(list)
-		self.marked = False
+    def __init__(self):
+        self.graph = defaultdict(list)
+        self.marked = False
 
-	def addEdge(self, u, v):
-		self.graph[u].append(v)
+    def addEdge(self, u, v):
+        self.graph[u].append(v)
 
-	def dfshelp(self, v, visited):
-		v.marked = True
-		for w in self.graph[v]:
-			if not w.marked:
-				self.dfshelp(w, visited)
+    def dfshelp(self, v, visited):
+        v.marked = True
+        for w in self.graph[v]:
+            if not w.marked:
+                self.dfshelp(w, visited)
 
-	def dfs(self, v):
-		visited = [False] * (len(self.graph))
-		self.dfshelp(v, visited)
+    def dfs(self, v):
+        visited = [False] * (len(self.graph))
+        self.dfshelp(v, visited)
 
-	def bfs(self, v):
-
-
-
-		return 1 
+    def bfs(self, v):
+        visited = [False] * len(self.graph)
+        queue = []
+        queue.append(v)
+        visited[v] = True
+        while queue:
+            v = queue.pop(0)
+            for n in self.graph[v]:
+                if not visited[n]:
+                    queue.append(n)
+                    visited[n] = True
