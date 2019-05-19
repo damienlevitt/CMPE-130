622dc30d85022accf1c5b64875bc0067f0dc37a9
Damien Levitt <damien.levitt@sjsu.edu>
2019-05-18 20:40:30 -0700

    hw 5 is complete

--- lib/hw5/hw5.py
+++ lib/hw5/hw5.py
@@ -1,4 +1,6 @@
 import sys
+from collections import defaultdict
+
 
 class Graph:
     def __init__(self, verticies):
@@ -36,15 +38,75 @@ class Edge:
         self.to_node = to_node
         self.length = length
 
-def dijkstra(graph, source):
-    dist = [sys.maxint] * sele
-    
-    return 1
+    def dijkstra(self, source):
+        dist = [sys.maxint] * self.V
+        dist[source] = 0
+        sptSet = [False] * self.V
 
-def BellmanFord(graph, source):
+        for cout in range(self.V):
+            u = self.minDistance(dist, sptSet)
+            sptSet[u] = True
+            for v in range(self.V):
+                if self.graph[u][v] > 0 and sptSet[v] is False and dist[v] > dist[u] + self.graph[u][v]:
+                    dist[v] = dist[u] + self.graph[u][v]
 
-    return 1
+    def BellmanFord(self, source):
+        dist = [float("Infinite")] * self.V
+        dist[source] = 0
 
-def Ford_fullerskon(graph, source, sink):    # you can implement Bfs or dfs to get the path from source(start node) to sink(end node)
+        for i in range(self.V - 1):
+            for u, v, w in self.graph:
+                if dist[u] != float("Infinite") and dist[u] + w < dist[v]:
+                    dist[v] = dist[u] + w
 
-    return 1 
+        for u, v, w in self.graph:
+            if dist[u] != float("Infinite") and dist[u] + w < dist[v]:
+                print("Graph contains negative weight cycle")
+                return
+
+    def Ford_fullerskon(graph, source, sink):    # you can implement Bfs or dfs to get the path from source(start node) to sink(end node)
+        def __init__(self, graph):
+            self.graph = graph  # residual graph
+            self.ROW = len(graph)
+            # self.COL = len(gr[0])
+
+        '''Returns true if there is a path from source 's' to sink 't' in 
+        residual graph. Also fills parent[] to store the path '''
+
+        def BFS(self, s, t, parent):
+
+            visited = [False] * (self.ROW)
+
+            queue = []
+
+            queue.append(s)
+            visited[s] = True
+
+            while queue:
+                u = queue.pop(0)
+                for ind, val in enumerate(self.graph[u]):
+                    if visited[ind] == False and val > 0:
+                        queue.append(ind)
+                        visited[ind] = True
+                        parent[ind] = u
+
+            return True if visited[t] else False
+
+        def FordFulkerson(self, source, sink):
+            parent = [-1] * (self.ROW)
+            max_flow = 0  # There is no flow initially
+            while self.BFS(source, sink, parent):
+                path_flow = float("Infinite")
+                s = sink
+                while (s != source):
+                    path_flow = min(path_flow, self.graph[parent[s]][s])
+                    s = parent[s]
+                max_flow += path_flow
+                v = sink
+                while (v != source):
+                    u = parent[v]
+                    self.graph[u][v] -= path_flow
+                    self.graph[v][u] += path_flow
+                    v = parent[v]
+
+            return max_flow
